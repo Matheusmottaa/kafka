b9aeace9d85f18add25e9386406693b37ac0cef2
Matheus <contact.mfmotta@gmail.com>
2025-01-09 22:57:37 -0300

    feature: adds consumer group and max poll for less chance of losing commits

--- src/main/java/com/mota/matheus/kafka/NewOrderMain.java
+++ src/main/java/com/mota/matheus/kafka/NewOrderMain.java
@@ -7,6 +7,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
@@ -14,32 +15,37 @@ public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
         String value = "71234,567,894";
-        String email = "Thanks! We are processing your order!";
 
-        ProducerRecord<String, String> record = new ProducerRecord<>(
-            "ECOMERCE_NEW_ORDER",
-                value,
-                value
-        );
+        for(int i=0; i<100; ++i) {
 
-        ProducerRecord<String, String> emailRecord = new ProducerRecord<>(
-                "ECOMERCE_SEND_EMAIL",
-                email,
-                email
-        );
+            String key = UUID.randomUUID().toString();
+            String email = "Thanks! We are processing your order!";
 
-        Callback callback = (data, ex) -> {
-            if(ex!=null) {
-                ex.printStackTrace();
-                return;
-            }
-            System.out.printf("Send with Success %s ::: partition %d / offset %d /%s%n",
-                    data.topic(), data.partition(), data.offset(), data.timestamp());
-        };
+            ProducerRecord<String, String> record = new ProducerRecord<>(
+                    "ECOMERCE_NEW_ORDER",
+                    key,
+                    value
+            );
 
+            ProducerRecord<String, String> emailRecord = new ProducerRecord<>(
+                    "ECOMERCE_SEND_EMAIL",
+                    key,
+                    email
+            );
 
-        producer.send(record, callback).get();     // metodo assincrono, chamando o metodo get voce espera o future terminar;
-        producer.send(emailRecord, callback).get();
+            Callback callback = (data, ex) -> {
+                if(ex!=null) {
+                    ex.printStackTrace();
+                    return;
+                }
+                System.out.printf("Send with Success %s ::: partition %d / offset %d /%s%n",
+                        data.topic(), data.partition(), data.offset(), data.timestamp());
+            };
+
+            producer.send(record, callback).get();     // metodo assincrono, chamando o metodo get voce espera o future terminar;
+            producer.send(emailRecord, callback).get();
+
+        }
     }
 
     public static Properties properties() {
